b67eee5a9fadbaf099fe95d74ed22bac94dc1429
mian muzammil <mianmuzammil30@gmail.com>
2025-10-28 10:25:14 +0500

    Fix: call not ending problem

---
+++ app/src/main/assets/call.js
@@ -0,0 +1,114 @@
+let localVideo = document.getElementById("local-video")
+let remoteVideo = document.getElementById("remote-video")
+
+localVideo.style.opacity = 0
+remoteVideo.style.opacity = 0
+
+localVideo.onplaying = () => { localVideo.style.opacity = 1 }
+remoteVideo.onplaying = () => { remoteVideo.style.opacity = 1 }
+let currentCall;
+let peer
+function init(userId) {
+    peer = new Peer(userId, {
+
+        port: 443,
+        path: '/'
+    })
+
+    peer.on('open', () => {
+        Android.onPeerConnected()
+    })
+
+    listen()
+}
+
+let localStream
+function listen() {
+    peer.on('call', (call) => {
+
+        navigator.getUserMedia({
+            audio: true, 
+            video: true
+        }, (stream) => {
+            localVideo.srcObject = stream
+            localStream = stream
+
+            call.answer(stream)
+
+             currentCall = call;
+
+            call.on('stream', (remoteStream) => {
+                remoteVideo.srcObject = remoteStream
+
+                remoteVideo.className = "primary-video"
+                localVideo.className = "secondary-video"
+
+            })
+
+        })
+        
+    })
+}
+
+function startCall(otherUserId) {
+    navigator.getUserMedia({
+        audio: true,
+        video: true
+    }, (stream) => {
+
+        localVideo.srcObject = stream
+        localStream = stream
+
+        const call = peer.call(otherUserId, stream)
+
+         currentCall = call;
+
+        call.on('stream', (remoteStream) => {
+            remoteVideo.srcObject = remoteStream
+
+            remoteVideo.className = "primary-video"
+            localVideo.className = "secondary-video"
+        })
+
+        call.on('close', () => {
+            stopLocalStream();
+            if (typeof Android !== 'undefined') {
+                Android.onPeerDisconnected();
+            }
+        });
+
+    })
+}
+
+function toggleVideo(b) {
+    if (b == "true") {
+        localStream.getVideoTracks()[0].enabled = true
+    } else {
+        localStream.getVideoTracks()[0].enabled = false
+    }
+} 
+
+function toggleAudio(b) {
+    if (b == "true") {
+        localStream.getAudioTracks()[0].enabled = true
+    } else {
+        localStream.getAudioTracks()[0].enabled = false
+    }
+}
+
+function stopLocalStream() {
+    if (localStream) {
+        localStream.getTracks().forEach(track => track.stop());
+        localStream = null;
+        localVideo.srcObject = null;
+    }
+}
+
+function endCall() {
+    if (currentCall) {
+        currentCall.close();
+        currentCall = null;
+    }
+    stopLocalStream();
+    remoteVideo.srcObject = null;
+}
